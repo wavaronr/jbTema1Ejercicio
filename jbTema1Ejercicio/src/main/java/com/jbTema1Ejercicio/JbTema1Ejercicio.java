2ffc267a81b89ced76b1c1938764bafb87cdbbcb
wavaronr <alexney1@hotmail.com>
2023-01-10 20:12:01 -0500

    jbTema1Ejercicio

---
+++ jbTema1Ejercicio/src/main/java/com/jbTema1Ejercicio/JbTema1Ejercicio.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.jbTema1Ejercicio;
+
+/**
+ *
+ * @author alexn
+ */
+public class JbTema1Ejercicio {
+
+    public static void main(String[] args) {
+        int entero= 1234567890;
+        long largo=  1234567890;
+        double doble= 2.525203;
+        boolean boleano = true;
+        String cadena= "Cadena de caracteres";
+        System.out.println(entero);
+        System.out.println(largo);
+        System.out.println(doble);
+        System.out.println(boleano);
+        System.out.println(cadena);
+        
+        
+        
+    }
+}
+
